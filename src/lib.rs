e2c29aab51026b3b5e3b6ad2c288ff9f731868b2
Masterchef365 <duncan.freeman1@gmail.com>
2023-05-17 14:19:11 -0700

    Many steps

--- src/lib.rs
+++ src/lib.rs
@@ -23,18 +23,27 @@ impl UserState for ClientState {
 
         sched.add_system(Self::update).build();
 
+        /*
         let mut sim = Sim::new(12_000);
         let n = sim.real.len();
 
-        for i in 0..n/18 {
+        for i in 0..n / 18 {
             sim.real[i] = 0.1;
             sim.imag[i] = 0.1;
         }
+        */
 
         //sim.real[n / 2] = 3.;
         //sim.real[1] = 1.;
 
-        let mut sim = wave_packet(DT * (n as f32 / 100.), 0., 1., 0.1, 1001);
+        let n = 1000;
+
+        let mut wave1 = wave_packet(DT * (n as f32 / 100.), 0., 1., 1.5, n);
+        let wave2 = wave_packet(DT * (n as f32 / 100.), 5., 1., 1.5, n);
+        wave1.real.iter_mut().zip(&wave2.real).for_each(|(a, b)| *a += b);
+        wave1.imag.iter_mut().zip(&wave2.imag).for_each(|(a, b)| *a += b);
+
+        let sim = wave1;
 
         Self(sim)
     }
@@ -43,7 +52,9 @@ impl UserState for ClientState {
 impl ClientState {
     fn update(&mut self, io: &mut EngineIo, _query: &mut QueryResult) {
         let Self(sim) = self;
-        sim.step();
+        for _ in 0..100 {
+            sim.step();
+        }
 
         io.send(&UploadMesh {
             id: WAVE_RDR,
@@ -79,14 +90,8 @@ impl Sim {
             let plus_1 = (i + 1) % n;
             let minu_1 = i.checked_sub(1).unwrap_or(n - 1);
 
-            self.imag[i] += c
-                * (self.real[plus_1]
-                    + self.real[minu_1]
-                    + 2. * self.real[i]);
-            self.real[i] -= c
-                * (self.imag[plus_1]
-                    + self.imag[minu_1]
-                    + 2. * self.imag[i]);
+            self.imag[i] += c * (self.real[plus_1] + self.real[minu_1] + 2. * self.real[i]);
+            self.real[i] -= c * (self.imag[plus_1] + self.imag[minu_1] + 2. * self.imag[i]);
         }
     }
 }
@@ -97,10 +102,7 @@ fn sim_to_mesh(sim: &Sim) -> Mesh {
     let n = sim.real.len();
     for i in 0..n {
         let f = i as f32 / n as f32;
-        let colors = [
-            (&sim.real, [1., 0., 0.]), 
-            (&sim.imag, [0., 0.5, 1.])
-        ];
+        let colors = [(&sim.real, [1., 0., 0.]), (&sim.imag, [0., 0.5, 1.])];
         for (part, color) in colors {
             let pos = [(f * TAU).cos(), part[i], (f * TAU).sin()];
             let idx = mesh.push_vertex(Vertex::new(pos, color));
@@ -108,11 +110,7 @@ fn sim_to_mesh(sim: &Sim) -> Mesh {
         }
 
         let mag = sim.real[i].powi(2) + sim.imag[i].powi(2);
-        let pos = [
-            (f * TAU).cos(),
-            mag + 1.,
-            (f * TAU).sin(),
-        ];
+        let pos = [(f * TAU).cos(), mag + 1., (f * TAU).sin()];
         let idx = mesh.push_vertex(Vertex::new(pos, [1.; 3]));
         mesh.push_indices(&[idx]);
     }
